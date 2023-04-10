98ff16cb8393c0297280cf8d761b00399d044f94
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-04-10 14:06:09 +0200

    Add Person class

---
+++ person.rb
@@ -0,0 +1,24 @@
+class Person
+  @@id = 1000
+
+  def initialize(age, name = 'Unknown', parent_permission = true)
+    @id = @@id
+    @@id += 1
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
