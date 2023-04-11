ee7d733d5cf344fbb8421d28590e369e09592963
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-04-11 13:13:48 +0200

    Create relo between Person and Book using Rental

--- person.rb
+++ person.rb
@@ -1,16 +1,17 @@
 require './nameable'
 
 class Person < Nameable
+  attr_reader :id
+  attr_accessor :name, :age, :rentals
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
-
-  attr_accessor :name, :age
-  attr_reader :id
 
   def can_use_services?
     of_age? || @parent_permission
@@ -18,6 +19,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals << rental
   end
 
   private
--- rental.rb
+++ rental.rb
@@ -1,7 +1,13 @@
 class Rental
   attr_accessor :date
 
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
+
+    @book = book
+    @book.add_rental(self)
+
+    @person = person
+    @person.add_rental(self)
   end
 end
